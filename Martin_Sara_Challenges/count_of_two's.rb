9f06d8e705dbfb46a9931c9cca247586ffc2b353
Matt Holmes <holmesm8@gmail.com>
2020-07-20 11:50:07 -0600

    Add problem description

--- Martin_Sara_Challenges/count_of_two's.rb
+++ Martin_Sara_Challenges/count_of_two's.rb
@@ -1,3 +1,14 @@
+# Write a function takes in a single integer and counts the number of 2's that appear in all the numbers between 0 and the given argument. The given number should be inclusive.
+
+# EXAMPLE:
+
+# Input: 25
+
+# Output: 9 (2, 12, 20, 21, 33, 23, 24, 25. 22 counts for two 2's)
+
+# Include at least two test cases to prove it works.
+
+
 def count_twos(int)
   count = 0
   for x in 0..int
